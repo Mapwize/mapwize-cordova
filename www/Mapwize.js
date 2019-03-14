bbad53b9005ac8ed04f03b9edac1380d5acd16d1
Laszlo Blum <laszlo.70.blum@gmail.com>
2019-03-14 13:08:57 +0100

    MapwizeView js class introduced

--- www/Mapwize.js
+++ www/Mapwize.js
@@ -4,28 +4,34 @@ var exec = cordova.exec;
 var PLUGIN_NAME = "Mapwize"
 
 function Mapwize() {
-	console.log("Mapwize.js: is created");
+	console.log("Mapwize: is created");
 }
-
-Mapwize.prototype.callbacks;
 
 Mapwize.prototype.createMapwizeView = function(options, success, failure) {
-	console.log("Mapwize.js: createMapwizeView");
+	console.log("Mapwize: createMapwizeView");
 	exec(function(result) { 
-				console.log("Mapwize.js: selectPlace: SUCCESS");
+				console.log("Mapwize: selectPlace: SUCCESS");
 				success(result);
 			}, function(err) {
-				console.log("Mapwize.js: selectPlace: FAILED");
+				console.log("Mapwize: selectPlace: FAILED");
 				failure(err);
 			}, PLUGIN_NAME, "createMapwizeView", [JSON.stringify(options)]);
+	return new MapwizeView();
 }
 
-Mapwize.prototype.setCallback = function(callbacks) {
-	console.log("Mapwize.js: createMapwizeView");
+
+function MapwizeView() {
+	console.log("MapwizeView: is created");
+}
+
+MapwizeView.prototype.callbacks;
+
+MapwizeView.prototype.setCallback = function(callbacks) {
+	console.log("MapwizeView: createMapwizeView");
 	this.callbacks = callbacks;
 
 	exec(function(result) { 
-		console.log("Mapwize.js: setCallback: SUCCESS");
+		console.log("MapwizeView: setCallback: SUCCESS");
 		switch(result.event) {
 			case "DidLoad":
 			case "DidTapOnFollowWithoutLocation":
@@ -46,52 +52,55 @@ Mapwize.prototype.setCallback = function(callbacks) {
 		}
 	}, function(result) {
 		/* alert("Error" + reply); */
-		console.log("Mapwize.js: setCallback: FAILED");}, PLUGIN_NAME, "setCallback", []);
+		console.log("MapwizeView: setCallback: FAILED");}, PLUGIN_NAME, "setCallback", []);
 }
 
-Mapwize.prototype.selectPlace = function(id, success, failure) {
-	console.log("Mapwize.js: selectPlace");
+MapwizeView.prototype.selectPlace = function(id, success, failure) {
+	console.log("MapwizeView: selectPlace");
 	exec(function(result) { 
-				console.log("Mapwize.js: selectPlace: SUCCESS");
+				console.log("MapwizeView: selectPlace: SUCCESS");
 				success(result);
 			}, function(err) {
-				console.log("Mapwize.js: selectPlace: FAILED");
+				console.log("MapwizeView: selectPlace: FAILED");
 				failure(err);
 			}, PLUGIN_NAME, "selectPlace", [id]);
 }
 
-Mapwize.prototype.selectPlaceList = function(id, success, failure) {
-	console.log("Mapwize.js: selectPlaceList");
+MapwizeView.prototype.selectPlaceList = function(id, success, failure) {
+	console.log("MapwizeView: selectPlaceList");
 	exec(function(result) { 
-				console.log("Mapwize.js: selectPlaceList: SUCCESS");
+				console.log("MapwizeView: selectPlaceList: SUCCESS");
 				success(result);
 			}, function(err) {
-				console.log("Mapwize.js: selectPlaceList: FAILED");
+				console.log("MapwizeView: selectPlaceList: FAILED");
 				failure(err);
 			}, PLUGIN_NAME, "selectPlaceList", [id]);
 }
 
-Mapwize.prototype.grantAccess = function(accessKey, success, failure) {
-	console.log("Mapwize.js: grantAccess");
+MapwizeView.prototype.grantAccess = function(accessKey, success, failure) {
+	console.log("MapwizeView: grantAccess");
 	exec(function(result) { 
-				console.log("Mapwize.js: grantAccess: SUCCESS");
+				console.log("MapwizeView: grantAccess: SUCCESS");
 				success(result);
 			}, function(err) {
-				console.log("Mapwize.js: grantAccess: FAILED");
+				console.log("MapwizeView: grantAccess: FAILED");
 				failure(err);
 			}, PLUGIN_NAME, "grantAccess", [accessKey]);
 }
 
-Mapwize.prototype.unselectContent = function(closeInfo, success, failure) {
-	console.log("Mapwize.js: unselectContent");
+MapwizeView.prototype.unselectContent = function(closeInfo, success, failure) {
+	console.log("MapwizeView: unselectContent");
 	exec(function(result) { 
-				console.log("Mapwize.js: unselectContent: SUCCESS");
+				console.log("MapwizeView: unselectContent: SUCCESS");
 				success(result);
 			}, function(err) {
-				console.log("Mapwize.js: unselectContent: FAILED");
+				console.log("MapwizeView: unselectContent: FAILED");
 				failure(err);
 			}, PLUGIN_NAME, "unselectContent", [closeInfo]);
 }
+
+
+
 
 var mapwize = new Mapwize();
 module.exports = mapwize;
