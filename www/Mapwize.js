6ea34ef19b0c0e48a0d4f5d8f1e35e05f944a862
Guillaume Emery <10091017+EmeryGuillaume@users.noreply.github.com>
2019-05-13 21:21:59 +0200

    fix mapwize create view (#24)

--- www/Mapwize.js
+++ www/Mapwize.js
@@ -6,24 +6,28 @@ function Mapwize() {
 	console.log("Mapwize: is created");
 }
 
-Mapwize.prototype.createMapwizeView = function(options, success, failure) {
+Mapwize.prototype.createMapwizeView = function (options, success, failure) {
 	console.log("Mapwize: createMapwizeView...");
-	exec(function(result) { 
-				console.log("Mapwize: createMapwizeView: SUCCESS ");
-				success(JSON.parse(result));
-			}, function(err) {
-				console.log("Mapwize: createMapwizeView: FAILED");
-				failure(err);
-			}, PLUGIN_NAME, "createMapwizeView", [JSON.stringify(options)]);
+	exec(function (result) {
+		console.log("Mapwize: selectPlace: SUCCESS");
+		if (typeof result === "object") {
+			success(JSON.parse(result));
+		} else {
+			success(result);
+		}
+	}, function (err) {
+		console.log("Mapwize: createMapwizeView: FAILED");
+		failure(err);
+	}, PLUGIN_NAME, "createMapwizeView", [JSON.stringify(options)]);
 	return new MapwizeView();
 }
 
-Mapwize.prototype.createOfflineManager = function(styleUrl, success, failure) {
+Mapwize.prototype.createOfflineManager = function (styleUrl, success, failure) {
 	console.log("Mapwize: createOfflineManager...");
 	return new OfflineManager(styleUrl);
 }
 
-Mapwize.prototype.createApiManager = function(success, failure) {
+Mapwize.prototype.createApiManager = function (success, failure) {
 	console.log("Mapwize: createApiManager...");
 	return new ApiManager();
 }
